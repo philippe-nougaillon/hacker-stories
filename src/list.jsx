22cef06733845bccba29b21ebcf42543ef186f1f
philippe-nougaillon <philippe.nougaillon@gmail.com>
2026-01-19 18:21:46 +0100

    column headers added

--- src/list.jsx
+++ src/list.jsx
@@ -1,5 +1,13 @@
-const List = ({ list, onRemoveItem }) =>
+const List = ({ list, onRemoveItem }) => 
     <ul>
+        <li style={{ display: 'flex' }}>
+            <span style={{ width: '40%' }}>Title</span>
+            <span style={{ width: '30%' }}>Author</span>
+            <span style={{ width: '10%' }}>Comments</span>
+            <span style={{ width: '10%' }}>Points</span>
+            <span style={{ width: '10%' }}>Actions</span>
+        </li>
+        <br></br>
         {list.map((item) => (
             <Item
                 key={item.objectID}
@@ -10,21 +18,20 @@ const List = ({ list, onRemoveItem }) =>
     </ul>
 
 const Item = ({ item, onRemoveItem }) =>
-    <>
-        <li>
-            <span>
-                <a href={item.url}>{item.title}</a>
-            </span>
-            <br></br>
-            <span>{item.author}</span>
-            <span>
-                <button type='button' onClick={() => onRemoveItem(item)}>
-                    Dismiss
-                </button>
-            </span>
-            <br></br>
-            <br></br>
-        </li>
-    </>  
+    <li style={{ display: 'flex'}}>
+        <span style={{ width: '40%' }}>
+            <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={{ width: '30%' }}>{item.author}</span>
+        <span style={{ width: '10%' }}>{item.num_comments}</span>
+        <span style={{ width: '10%' }}>{item.points}</span>
+        <span style={{ width: '10%' }}>
+            <button type='button' onClick={() => onRemoveItem(item)}>
+                Dismiss
+            </button>
+        </span>
+        <br></br>
+        <br></br>
+    </li>
 
 export { List };
