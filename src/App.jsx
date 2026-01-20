ab536f676ec7e57726275dd7d3fc383131b70eab
philippe-nougaillon <philippe.nougaillon@gmail.com>
2026-01-20 19:03:33 +0100

    components moved in one place

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import './App.css'
 import * as React from 'react';
 import axios from 'axios';
-import { SearchForm } from './search-form';
+import { SearchForm } from './components/search-form';
 import { List } from './list';
-import { LastSearches } from './last-searches';
+import { LastSearches } from './components/last-searches';
 
-const API_BASE = 'https://hn.algolia.com/api/v1';
-const API_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
+const API_BASE      = 'https://hn.algolia.com/api/v1';
+const API_SEARCH    = '/search';
+const PARAM_SEARCH  = 'query=';
+const PARAM_PAGE    = 'page=';
 
 const getUrl = (searchTerm, page) => `${API_BASE}${API_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`;
 
-const extractSearchTerm = (url) => 
+const extractSearchTerm = (url) =>
   url
     .substring(url.lastIndexOf('?') + 1, url.lastIndexOf('&'))
     .replace(PARAM_SEARCH, '');
 
-const getLastSearches = (urls) => 
+const getLastSearches = (urls) =>
   urls.reduce((result, url, index) => {
     const searchTerm = extractSearchTerm(url);
     if (index === 0) {
@@ -32,8 +32,8 @@ const getLastSearches = (urls) =>
       return result.concat(searchTerm);
     }
   }, [])
-  .slice(-6)
-  .slice(0, -1);
+    .slice(-6)
+    .slice(0, -1);
 
 const App = () => {
 
@@ -50,10 +50,10 @@ const App = () => {
           ...state,
           isLoading: false,
           isError: false,
-          data: 
+          data:
             action.payload.page === 0
-            ? action.payload.list
-            : state.data.concat(action.payload.list),
+              ? action.payload.list
+              : state.data.concat(action.payload.list),
           page: action.payload.page,
         };
       case 'STORIES_FETCH_FAILURE':
@@ -85,12 +85,13 @@ const App = () => {
     try {
       const lastUrl = urls[urls.length - 1];
       const result = await axios.get(lastUrl);
-      dispatchStories({ type: 'STORIES_FETCH_SUCCESS', 
-                        payload: {
-                          list: result.data.hits, 
-                          page: result.data.page, 
-                        }
-                      });
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: {
+          list: result.data.hits,
+          page: result.data.page,
+        }
+      });
     } catch (error) {
       dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
     }
@@ -122,7 +123,6 @@ const App = () => {
 
   const lastSearches = getLastSearches(urls);
 
-
   React.useEffect(() => {
     handleFetchStories();
   }, [handleFetchStories]);
@@ -146,7 +146,7 @@ const App = () => {
         lastSearches={lastSearches}
         onLastSearch={handleLastSearch}
       />
-      
+
       <hr />
 
       {stories.isError && <p>Oops... something went wrong!</p>}
@@ -159,7 +159,7 @@ const App = () => {
         <button type='button' onClick={handleMore}>
           More
         </button>
-      )}  
+      )}
     </>
   )
 };
