e1faba2993d527a8752c9677f2262b33bd416a89
philippe-nougaillon <philippe.nougaillon@gmail.com>
2026-01-20 15:27:17 +0100

    button added to fetch more content

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css'
 import * as React from 'react';
 import axios from 'axios';
-
-
 import { SearchForm } from './search-form';
 import { List } from './list';
 import { LastSearches } from './last-searches';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
-const extractSearchTerm = (url) => url.replace(API_ENDPOINT, '');
-const getUrl = (searchTerm) => `${API_ENDPOINT}${searchTerm}`;
+const API_BASE = 'https://hn.algolia.com/api/v1';
+const API_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+
+const getUrl = (searchTerm, page) => `${API_BASE}${API_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`;
+
+const extractSearchTerm = (url) => {
+  console.log('URL => ' + url);
+
+  url
+    .substring(url.lastIndexOf('?') + 1, url.lastIndexOf('&'))
+    .replace(PARAM_SEARCH, '');
+
+}
 
 const getLastSearches = (urls) => 
   urls.reduce((result, url, index) => {
@@ -44,7 +55,11 @@ const App = () => {
           ...state,
           isLoading: false,
           isError: false,
-          data: action.payload,
+          data: 
+            action.payload.page === 0
+            ? action.payload.list
+            : state.data.concat(action.payload.list),
+          page: action.payload.page,
         };
       case 'STORIES_FETCH_FAILURE':
         return {
@@ -62,9 +77,9 @@ const App = () => {
     }
   };
 
-  const [stories, dispatchStories] = React.useReducer(storiesReducer, { data: [], isLoading: false, isError: false });
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, { data: [], page: 0, isLoading: false, isError: false });
   const [searchTerm, setSearchTerm] = React.useState(localStorage.getItem('search') || '');
-  const [urls, setUrls] = React.useState([getUrl(searchTerm)]);
+  const [urls, setUrls] = React.useState([getUrl(searchTerm, 0)]);
 
   const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
@@ -75,7 +90,12 @@ const App = () => {
     try {
       const lastUrl = urls[urls.length - 1];
       const result = await axios.get(lastUrl);
-      dispatchStories({ type: 'STORIES_FETCH_SUCCESS', payload: result.data.hits });
+      dispatchStories({ type: 'STORIES_FETCH_SUCCESS', 
+                        payload: {
+                          list: result.data.hits, 
+                          page: result.data.page, 
+                        }
+                      });
     } catch (error) {
       dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
     }
@@ -85,18 +105,24 @@ const App = () => {
     dispatchStories({ type: 'REMOVE_STORY', payload: item });
   };
 
-  const handleSearch = (searchTerm) => {
-    const url = getUrl(searchTerm);
+  const handleSearch = (searchTerm, page) => {
+    const url = getUrl(searchTerm, page);
     setUrls(urls.concat(url));
   };
 
   const handleLastSearch = (searchTerm) => {
     setSearchTerm(searchTerm);
-    handleSearch(searchTerm);
+    handleSearch(searchTerm, 0);
   };
 
-  const searchAction = (event) => {
-    handleSearch(searchTerm);
+  const handleMore = () => {
+    const lastUrl = urls[urls.length - 1];
+    const searchTerm = extractSearchTerm(lastUrl);
+    handleSearch(searchTerm, stories.page + 1);
+  };
+
+  const searchAction = () => {
+    handleSearch(searchTerm, 0);
   };
 
   const lastSearches = getLastSearches(urls);
@@ -135,6 +161,10 @@ const App = () => {
       ) : (
         <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
+
+      <button type='button' onClick={handleMore}>
+        More
+      </button>
     </>
   )
 };
